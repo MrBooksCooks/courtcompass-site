a31d5f2098eb27e20b6dc2e6c76ecf287bee6f03
MrBooksCooks <sales@belfastbooks.co.uk>
2026-05-13 13:30:35 +0100

    Update

--- worker.js
+++ worker.js
@@ -1,6 +1,15 @@
 export default {
   async fetch() {
     const r = await fetch('https://raw.githubusercontent.com/MrBooksCooks/courtcompass-site/main/index.html');
-    return new Response(await r.text(), { headers: { 'Content-Type': 'text/html;charset=UTF-8' } });
+    return new Response(await r.text(), {
+      headers: {
+        'Content-Type': 'text/html;charset=UTF-8',
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Referrer-Policy': 'strict-origin-when-cross-origin',
+        'Permissions-Policy': 'camera=(), microphone=(), geolocation=()',
+        'Strict-Transport-Security': 'max-age=31536000; includeSubDomains; preload',
+      }
+    });
   }
 };
